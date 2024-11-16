5726466f0b12387ad9bcabefcc02f559ef55f303
LitiaEeloo <litiaeeloo@gmail.com>
2024-11-16 04:17:59 -0500

    repo: init

---
+++ client/src/extension.ts
@@ -0,0 +1,78 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import {
+  languages,
+  workspace,
+  EventEmitter,
+  ExtensionContext,
+  window,
+  InlayHintsProvider,
+  TextDocument,
+  CancellationToken,
+  Range,
+  InlayHint,
+  TextDocumentChangeEvent,
+  ProviderResult,
+  commands,
+  WorkspaceEdit,
+  TextEdit,
+  Selection,
+  Uri,
+} from "vscode";
+
+import {
+  Disposable,
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+// type a = Parameters<>;
+
+export async function activate(context: ExtensionContext) {
+  const traceOutputChannel = window.createOutputChannel("LALRPOP Language Server trace");
+  const command = process.env.SERVER_PATH || "lalrpop-lsp";
+  const run: Executable = {
+    command,
+    options: {
+      env: {
+        ...process.env,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        RUST_LOG: "debug",
+      },
+    },
+  };
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "LALRPOP" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+    traceOutputChannel,
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient("lalrpop-lsp", "LALRPOP language server", serverOptions, clientOptions);
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
+
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use lalrpop::lsp::*;
+use tower_lsp::{LspService, Server};
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let (service, socket) = LspService::build(LalrpopLsp::new).finish();
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
